d4a4a2c5355042e6c52ad61a9e63b65d45505b76
Kosala Wijesena <kosalabakk@yahoo.co.uk>
2021-05-25 19:13:47 +0200

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ ALL
+  get "tasks", to: "tasks#index"
+  # # READ ONE
+  get "tasks/:id", to: "tasks#show", as: "task"
+  # # restaurant_path(:id)
 end
